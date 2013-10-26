94d45c69d6b8dd3ba0e2c42a95d64308ae573249
Miek Gieben <miek@miek.nl>
2013-10-26 15:27:19 +0100

    Some working ratelimiting

--- ratelimit/as112.go
+++ ratelimit/as112.go
@@ -22,43 +22,86 @@ import (
 	"time"
 )
 
-const RATESIZE = 10000
+const (
+	WINDOW = 5
+	BUCKETSIZE = 10000
+	LIMIT = 50
+)
 
 type bucket struct {
 	source net.Addr  // client address
 	stamp  time.Time // time of last count update
 	rate   int       // rate of the queries for this client
 	count  int       // number of requests seen in the last secnd
-	block  int	 // should we block it
 }
 
-type blocker [RATESIZE]*bucket
-
-func (b blocker) Count(a net.Addr, q, r *dns.Msg) {
-	offset := adler32.Checksum(a.(*net.IPAddr).IP) % RATESIZE
-	if b[offset] == nil { // re-initialize of source differs?
-		b[offset] = &bucket{a, time.Now(), 0, 1, 0}
-		b[offset].block = 0
-		return
-	}
-	if time.Since(b[offset].stamp) < time.Second {
-		b[offset].count++
-		b[offset].block = 0
-		return
-	}
-	if time.Since(b[offset].stamp) > 16 * time.Second {
-		b[offset].rate = 0
-		b[offset].count = 1
-		b[offset].block = 0
-		return
-	}
-	b[offset].rate >>= uint(time.Since(b[offset].stamp).Seconds())
-	return
+type request struct {
+	a net.Addr
+	q *dns.Msg
+	r *dns.Msg
 }
 
-func (b blocker) Blocked(a net.Addr, q *dns.Msg) int {
+type blocker struct {
+	block [BUCKETSIZE]*bucket
+	ch    chan *request
+}
+
+// serialize the writing.
+func (b *blocker) blockerUpdate() {
+	offset := 0
+	for {
+		select {
+		case r := <-b.ch:
+			if t, ok := r.a.(*net.UDPAddr); ok {
+				offset = int(adler32.Checksum(t.IP) % BUCKETSIZE)
+			}
+			if t, ok := r.a.(*net.TCPAddr); ok {
+				offset = int(adler32.Checksum(t.IP) % BUCKETSIZE)
+			}
+			if b.block[offset] == nil { // re-initialize if source differs?
+				b.block[offset] = &bucket{r.a, time.Now(), 0, 1}
+				continue
+			}
+			if time.Since(b.block[offset].stamp) < time.Second {
+				b.block[offset].stamp = time.Now()
+				b.block[offset].count++
+				b.block[offset].rate = b.block[offset].count
+				continue
+			}
+			if time.Since(b.block[offset].stamp) > WINDOW*time.Second {
+				b.block[offset].stamp = time.Now()
+				b.block[offset].rate = 0
+				b.block[offset].count = 1
+				continue
+			}
+			b.block[offset].rate >>= uint(time.Since(b.block[offset].stamp).Seconds())
+			b.block[offset].rate += b.block[offset].count
+			b.block[offset].stamp = time.Now()
+			b.block[offset].count = 1
+		}
+	}
+}
+
+func (b *blocker) Count(a net.Addr, q, r *dns.Msg) {
+	b.ch <- &request{a, q, r}
+}
+
+func (b *blocker) Block(a net.Addr, q *dns.Msg) int {
+	offset := 0
+	if t, ok := a.(*net.UDPAddr); ok {
+		offset = int(adler32.Checksum(t.IP) % BUCKETSIZE)
+	}
+	if t, ok := a.(*net.TCPAddr); ok {
+		offset = int(adler32.Checksum(t.IP) % BUCKETSIZE)
+	}
+	if b.block[offset] == nil {
+		return 0
+	}
+	if b.block[offset].rate > LIMIT {
+		println("HITTING LIMIT, THROTTLING")
+		return -1
+	}
 	return 0
-
 }
 
 const SOA string = "@ SOA prisoner.iana.org. hostmaster.root-servers.org. 2002040800 1800 900 0604800 604800"
@@ -98,6 +141,8 @@ func main() {
 		pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
 	}
+	b := &blocker{ch: make(chan *request, 10000)}
+	go b.blockerUpdate()
 	for z, rr := range zones {
 		rrx := rr.(*dns.SOA) // Needed to create the actual RR, and not an reference.
 		dns.HandleFunc(z, func(w dns.ResponseWriter, r *dns.Msg) {
@@ -105,18 +150,19 @@ func main() {
 			m.SetReply(r)
 			m.Authoritative = true
 			m.Ns = []dns.RR{rrx}
+			b.Count(w.RemoteAddr(), m, r)
 			w.WriteMsg(m)
 		})
 	}
 	go func() {
-		srv := &dns.Server{Addr: ":8053", Net: "tcp", Ratelimiter: new(blocker)}
+		srv := &dns.Server{Addr: ":8053", Net: "tcp", Ratelimiter: b}
 		err := srv.ListenAndServe()
 		if err != nil {
 			log.Fatal("Failed to set tcp listener %s\n", err.Error())
 		}
 	}()
 	go func() {
-		srv := &dns.Server{Addr: ":8053", Net: "udp", Ratelimiter: new(blocker)}
+		srv := &dns.Server{Addr: ":8053", Net: "udp", Ratelimiter: b}
 		err := srv.ListenAndServe()
 		if err != nil {
 			log.Fatal("Failed to set udp listener %s\n", err.Error())
